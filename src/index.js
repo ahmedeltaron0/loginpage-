99ba5fce8beb25801493c1219c3493d737102ed9
Ahmed Elsayed Eltaron <96958864+ahmedeltaron0@users.noreply.github.com>
2023-12-22 00:39:11 +0200

    app connected with mongodb and nodejs

---
+++ src/index.js
@@ -0,0 +1,84 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+// const hbs = require("hbs")
+const LogInCollection = require("./mongo")
+const port = process.env.PORT || 3000
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }))
+
+const tempelatePath = path.join(__dirname, '../tempelates')
+const publicPath = path.join(__dirname, '../public')
+console.log(publicPath);
+
+app.set('view engine', 'hbs')
+app.set('views', tempelatePath)
+app.use(express.static(publicPath))
+
+
+// hbs.registerPartials(partialPath)
+
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+app.get('/', (req, res) => {
+    res.render('login')
+})
+
+
+
+// app.get('/home', (req, res) => {
+//     res.render('home')
+// })
+
+app.post('/signup', async (req, res) => {
+    const { name, password } = req.body;
+
+    try {
+        const existingUser = await LogInCollection.findOne({ name });
+
+        if (existingUser) {
+            res.send("User already exists");
+        } else {
+            await LogInCollection.create({ name, password });
+            res.status(201).render("home", { naming: name });
+        }
+    } catch (error) {
+        res.send("Error creating user");
+    }
+});
+
+
+app.post('/login', async (req, res) => {
+
+    try {
+        const check = await LogInCollection.findOne({ name: req.body.name })
+
+        if (check.password === req.body.password) {
+            res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` })
+        }
+
+        else {
+            res.send("incorrect password")
+        }
+
+
+    } 
+    
+    catch (e) {
+
+        res.send("wrong details")
+        
+
+    }
+
+
+})
+
+
+
+app.listen(port, () => {
+    console.log('port connected');
+})
